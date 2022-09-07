50403918b8be222bde84612361061f3ba64d2d02
Maria Erika <erikasaleslim@gmail.com>
2022-09-07 12:10:31 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1></h1>
+  return (
+    <>
+      <h1></h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
